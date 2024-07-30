19e9b2b6db5d6206adc06d2697cea19e99749a51
fieldsusers <alezapellido@gmail.com>
2024-07-29 21:42:39 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+const respuestas = [
+  "No me quedaron productos lo siento",
+  "Eres menor de edad para la consumición lo siento",
+  "toma y no digas nada ",
+  "2 kilitos solo para ti disfruta",
+  "1 gramo humilde disfruta",
+  "No tengo "
+];
+
+const random = Math.floor(Math.random() * respuestas.length);
+console.log(respuestas[random]);
